9ea74ff22744a6d86efaa8e65b7f5bb4e02f3643
Dhanashree Rane <dhanashreerane18@gmail.com>
2020-12-30 14:49:51 +0530

    forth page

---
+++ Desktop/Narendra project/Happn Project/app/src/main/java/com/example/happpn/MainActivity4.java
@@ -0,0 +1,25 @@
+package com.example.happpn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity4 extends AppCompatActivity {
+    private Button mainActivityJava4BtnContinue;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+        mainActivityJava4BtnContinue = findViewById(R.id.mainActivity4btnContinue);
+        mainActivityJava4BtnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentmainActivity4 = new Intent(MainActivity4.this,MainActivity5.class);
+                startActivity(intentmainActivity4);
+            }
+        });
+    }
+    }
