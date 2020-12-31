eb5f9ac19c5d95cbf23ec5b1777c28adac47e768
narendra panchal <narendrapanchal020@gmail.com>
2020-12-31 17:27:50 +0530

    inetentdone till 10

--- Desktop/Narendra project/Happn Project/app/src/main/java/com/example/happpn/MainActivity6.java
+++ Desktop/Narendra project/Happn Project/app/src/main/java/com/example/happpn/MainActivity6.java
@@ -8,11 +8,19 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity6 extends AppCompatActivity {
-
+    private Button mainActivity6mbtnLetsGo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main6);
+        mainActivity6mbtnLetsGo=findViewById(R.id.mainActivity6btnLetsGo);
+        mainActivity6mbtnLetsGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentmainActivity6=new Intent(MainActivity6.this,MainActivity8.class);
+                startActivity(intentmainActivity6);
+            }
+        });
 
 
     }
