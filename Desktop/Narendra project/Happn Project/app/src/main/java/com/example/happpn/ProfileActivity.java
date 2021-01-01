a14bdaed363b9374c6852075e15ac38122b1c3cf
Dhanashree Rane <dhanashreerane18@gmail.com>
2021-01-01 14:34:11 +0530

    Activity profile completed

--- Desktop/Narendra project/Happn Project/app/src/main/java/com/example/happpn/ProfileActivity.java
+++ Desktop/Narendra project/Happn Project/app/src/main/java/com/example/happpn/ProfileActivity.java
@@ -3,15 +3,12 @@ package com.example.happpn;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 
-public class MainActivity13 extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main13);
-
+        setContentView(R.layout.activity_profile);
     }
 }
