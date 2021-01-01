c36b78c8a1a7446176ca97bffc40369a81b1f74c
narendra panchal <narendrapanchal020@gmail.com>
2021-01-01 11:58:45 +0530

    intent part

--- Desktop/Narendra project/Happn Project/app/src/main/java/com/example/happpn/MainActivity2.java
+++ Desktop/Narendra project/Happn Project/app/src/main/java/com/example/happpn/MainActivity2.java
@@ -6,20 +6,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
-private Button mainActivityJava2BtnContinue;
+    private Button mainActivityJava2BtnContinue;
+    private EditText mainActivity2mtextPhoneNumber;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-            mainActivityJava2BtnContinue = findViewById(R.id.mainActivity2btnContinue);
-            mainActivityJava2BtnContinue.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intentmainActivity2 = new Intent(MainActivity2.this,MainActivity3.class);
-                    startActivity(intentmainActivity2);
-                }
-            });
+        isCredentialValid();
+        mainActivityJava2BtnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isCredentialValid()){
+                Intent intentmainActivity2 = new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(intentmainActivity2);
+            }}
+        });
     }
+            private Boolean isCredentialValid(){
+            Boolean isDatavalid=true;
+                mainActivityJava2BtnContinue = findViewById(R.id.mainActivity2btnContinue);
+                mainActivity2mtextPhoneNumber=findViewById(R.id.mainActivity2textPhoneNumber);
+            if(mainActivity2mtextPhoneNumber.getText().toString().length()!=10){
+                mainActivity2mtextPhoneNumber.setError("Not a valid number");
+                isDatavalid=false;
+            }
+
+           return isDatavalid;
+            }
+
+
 }
