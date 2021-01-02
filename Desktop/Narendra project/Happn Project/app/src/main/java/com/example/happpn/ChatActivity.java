a2b04eb4524e31459468cfa9b87c44829fe2967c
narendra panchal <narendrapanchal020@gmail.com>
2021-01-02 10:54:41 +0530

    completed

--- Desktop/Narendra project/Happn Project/app/src/main/java/com/example/happpn/ChatActivity.java
+++ Desktop/Narendra project/Happn Project/app/src/main/java/com/example/happpn/ChatActivity.java
@@ -2,13 +2,43 @@ package com.example.happpn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ChatActivity extends AppCompatActivity {
-
+ private Button activityChatmProfile;
+ private Button activityChatmNotification;
+ private Button activityChatmHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        activityChatmProfile=findViewById(R.id.activityChatProfile);
+        activityChatmNotification=findViewById(R.id.activityChatNotification);
+        activityChatmHome=findViewById(R.id.activityChatHome);
+        activityChatmProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inetntactivityChatP=new Intent(ChatActivity.this,ProfileActivity.class);
+                startActivity(inetntactivityChatP);
+            }
+        });
+        activityChatmNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inetntactivityChatN=new Intent(ChatActivity.this,Notification.class);
+                startActivity(inetntactivityChatN);
+            }
+        });
+        activityChatmHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inetntactivityChatH=new Intent(ChatActivity.this,MainActivity13.class);
+                startActivity(inetntactivityChatH);
+            }
+        });
     }
 }
--- Desktop/Narendra project/Happn Project/app/src/main/java/com/example/happpn/MainActivity5.java
+++ Desktop/Narendra project/Happn Project/app/src/main/java/com/example/happpn/MainActivity5.java
@@ -13,19 +13,43 @@ import static java.lang.Integer.parseInt;
 
 public class MainActivity5 extends AppCompatActivity {
     private Button mainActivity5btn;
+    private EditText mainActivity5mDay;
+    private EditText mainActivity5mMonth;
+    private EditText mainActivity5mYear;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+        isCredentialValid();
         mainActivity5btn=findViewById(R.id.mainActivity5btnContinue);
         mainActivity5btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+            if(isCredentialValid()){
                 Intent inetntmainActivity5=new Intent(MainActivity5.this,MainActivity6.class);
                 startActivity(inetntmainActivity5);
-          }
+          }}
         });
+   }
+
+   private Boolean isCredentialValid(){
+        Boolean isDataValid=true;
+        mainActivity5mMonth=findViewById(R.id.mainActivity5Month);
+        mainActivity5mYear=findViewById(R.id.mainActivity5Year);
+        mainActivity5mDay=findViewById(R.id.mainActivity5Day);
+        if(mainActivity5mDay.getText().toString().isEmpty()){
+            mainActivity5mDay.setError("Invalid Date");
+            isDataValid=false;
+        }
+        if(mainActivity5mYear.getText().toString().isEmpty()){
+           mainActivity5mYear.setError("Invalid Year");
+           isDataValid=false;
+       }
+        if(mainActivity5mMonth.getText().toString().isEmpty()){
+            mainActivity5mMonth.setError("Invalid Month");
+            isDataValid=false;
+        }
+        return isDataValid;
    }
 
 }
